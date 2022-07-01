742a951f9e6cceefb3c8f9691bb34e95448a214c
luswar <luswar@163.com>
2022-07-01 15:11:31 +0800

    Fix

--- reward_point/BasicAuth.sol
+++ reward_point/BasicAuth.sol
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * */
-
 pragma solidity ^0.4.25;
 
 contract BasicAuth {
@@ -23,18 +7,15 @@ contract BasicAuth {
         _owner = msg.sender;
     }
 
-    modifier onlyOwner() { 
+    modifier onlyOwner() {
         require(auth(msg.sender), "Only owner!");
-        _; 
+        _;
     }
 
-    function setOwner(address owner)
-        public
-        onlyOwner
-    {
+    function setOwner(address owner) public onlyOwner {
         _owner = owner;
     }
-    
+
     function auth(address src) public view returns (bool) {
         if (src == address(this)) {
             return true;
